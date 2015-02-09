ce19c167261f31585442662ac72e26fa8109c445
Katrina Cruz <kaysabelle@gmail.com>
2015-02-09 10:42:40 -0800

    daily weather update

--- commit.sh
+++ commit.sh
@@ -1,7 +1,12 @@
 #!/bin/bash          
 
+#echo Adding private key...
+exec ssh-agent bash
+ssh-add ~/.ssh/id_rsa
+echo Pulling latest weather data...
 sudo git pull
 sudo python weatherScrapper.py
 sudo git add -A
 sudo git commit -m "Daily Weather Update"
 sudo git push 
+echo All done!
